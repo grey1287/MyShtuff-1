c617285d3d2b030fefc9c19a4c76d1811fbfdbe9
Jang <sbtan1989@gmail.com>
2021-01-07 14:39:21 -0800

    Finally

--- main_app/views.py
+++ main_app/views.py
@@ -15,6 +15,7 @@ from django.urls import reverse, reverse_lazy
 def home(request):
     return render(request, 'home.html')
 
+
 def signup(request):
     error_message = ''
     if request.method == 'POST':
@@ -29,60 +30,70 @@ def signup(request):
     context = {'form': form, 'error_message': error_message}
     return render(request, 'registration/signup.html', context)
 
+
 class ShtuffListList(LoginRequiredMixin, ListView):
     model = ShtuffList
 
+
 class ShtuffListCreate(LoginRequiredMixin, CreateView):
     model = ShtuffList
-    fields = ['name','description','image']
+    fields = ['name', 'description', 'image']
 
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
 
+
 class ShtuffListDelete(LoginRequiredMixin, DeleteView):
     model = ShtuffList
     success_url = '/shtuff_lists/'
 
+
 class ShtuffListUpdate(LoginRequiredMixin, UpdateView):
     model = ShtuffList
-    fields = ['description','image']
+    fields = ['description', 'image']
+
 
 @login_required
 def shtuff_list_detail(request, shtuff_list_id):
-    shtuff_list = ShtuffList.objects.get(id = shtuff_list_id)
+    shtuff_list = ShtuffList.objects.get(id=shtuff_list_id)
     shtuffs = Shtuff.objects.filter(shtuff_list=shtuff_list_id)
     return render(request, 'shtuff_lists/shtuff_lists_detail.html', {
-        'shtuff_list' : shtuff_list,
-        'shtuffs' : shtuffs
+        'shtuff_list': shtuff_list,
+        'shtuffs': shtuffs
     })
 
-class ShtuffCreate(LoginRequiredMixin,CreateView):
+
+class ShtuffCreate(LoginRequiredMixin, CreateView):
     model = Shtuff
-    fields = ['name','description', 'price', 'image', 'url']
+    fields = ['name', 'description', 'price', 'image', 'url']
 
     def form_valid(self, form):
-        new_shtuff = form.save(commit = False)
+        new_shtuff = form.save(commit=False)
         new_shtuff.shtuff_list = ShtuffList.objects.get(id=self.kwargs['pk'])
         new_shtuff.save()
         return super().form_valid(form)
 
-class ShtuffDelete(LoginRequiredMixin,DeleteView):
+
+class ShtuffDelete(LoginRequiredMixin, DeleteView):
     model = Shtuff
 
     def get_success_url(self):
         return reverse('shtuff_list_detail', kwargs={'shtuff_list_id': self.object.shtuff_list.id})
 
-class ShtuffUpdate(LoginRequiredMixin,UpdateView):
+
+class ShtuffUpdate(LoginRequiredMixin, UpdateView):
     model = Shtuff
     fields = ['description', 'price', 'image', 'url']
 
+
 @login_required
 def shtuff_detail(request, shtuff_id):
-    shtuff = Shtuff.objects.get(id = shtuff_id)
+    shtuff = Shtuff.objects.get(id=shtuff_id)
     return render(request, 'shtuff/shtuff_detail.html', {
-        'shtuff' : shtuff,
+        'shtuff': shtuff,
     })
+
 
 @login_required
 def profile(request):
